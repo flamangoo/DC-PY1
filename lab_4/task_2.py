7c98dbbd730f6aefa7ed670d07928721e272fd9a
Kristina Lyutostanskaya <115110092+flamangoo@users.noreply.github.com>
2022-10-28 19:36:19 +0300

    Update task_2.py

--- lab_4/task_2.py
+++ lab_4/task_2.py
@@ -13,7 +13,7 @@ def get_percents(char_dictionary):  # функция принимает слов
     percents = {}
     for char in char_dictionary:
         percents[char] = char_dictionary[char] / sum_of_values * 100
-        error = 100 - sum(percents.values())  # нахождение отклонения от 100%
+    error = 100 - sum(percents.values())  # нахождение отклонения от 100%
     percents.update({'err': error})  # добавим ошибку как дополнительный ключ
     # print(sum(percents.values()))
     # print(error)
