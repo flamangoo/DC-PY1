30d71cd6e64f52b9a71178eed7a6049b75a3893c
Kristina Lyutostanskaya <115110092+flamangoo@users.noreply.github.com>
2022-11-04 15:12:43 +0300

    Update task_5.py

--- lab5/task_5.py
+++ lab5/task_5.py
@@ -3,8 +3,8 @@ from random import sample
 
 
 def get_random_password(n: int = 8) -> str:
-    random_str = ascii_lowercase + ascii_uppercase + digits
-    password = ''.join(sample(random_str, n))
+    selection_str = ascii_lowercase + ascii_uppercase + digits
+    password = ''.join(sample(selection_str, n))
     return password
 
 
