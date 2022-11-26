a9d22f23d3408941116220f815815e39a5c61949
Kristina Lyutostanskaya <115110092+flamangoo@users.noreply.github.com>
2022-11-27 01:32:15 +0300

    Update task_4.py

--- lab6/task_4.py
+++ lab6/task_4.py
@@ -1,18 +1,32 @@
-import json
+INPUT_FILE = 'input.csv'
+OUTPUT_FILE = 'output_json'
 
-INPUT_FILE = "input.csv"
+indent = '    '
 
 
-def csv_to_list_dict(filename, delimiter=',', new_line='\n') -> list[dict]:
-    with open(filename, 'r', encoding='utf-8') as file:
-        form = []
+def csv_to_list_dict(input_file, new_line='\n', delimiter=',') -> list[dict]:
+    with open(input_file, 'r', encoding='utf-8') as file:
+        header = file.readline().replace(new_line, '').split(delimiter)
         for line in file:
-            line = line.replace(new_line, '')  # убираем символ переноса строки
-            form.append(line.split(delimiter))  # формируем список по разделителю
-    for i in range(len(form)):
-        form[i] = dict(zip(form[0], form[i]))  # объединение элементов
-
-    return form
+            line = line.replace(new_line, '')
+            yield dict(zip(header, line.split(delimiter)))
 
 
-print(json.dumps(csv_to_list_dict(INPUT_FILE)[1:], indent=4, ensure_ascii=False))
+# save dict as json record
+def converter_csv_to_json(input_file, output_file, new_line='\n', delimiter=','):
+    with open('output.json', 'w', encoding='utf-8') as serial:
+        serial.write('[' + new_line)
+        is_first_row = True  # flag
+        for record in csv_to_list_dict(input_file):
+            if not is_first_row:
+                serial.write(delimiter + new_line)
+            serial.write(indent + '{' + new_line)
+            is_first_row = True
+            for key, value in record.items():
+                if not is_first_row:
+                    serial.write(delimiter + new_line)
+                serial.write(f'{indent}{indent}"{key}": {value}')
+                is_first_row = False
+            serial.write(new_line + indent + '}')
+            is_first_row = False
+        serial.write(new_line + ']')
