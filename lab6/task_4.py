6552804072714dbee29ec3c8faa98134df0c3336
Kristina Lyutostanskaya <115110092+flamangoo@users.noreply.github.com>
2022-11-27 17:52:26 +0300

    Update task_4.py

--- lab6/task_4.py
+++ lab6/task_4.py
@@ -17,16 +17,20 @@ def converter_csv_to_json(input_file, output_file, new_line='\n', delimiter=',')
     with open(output_file, 'w', encoding='utf-8') as serial:
         serial.write('[' + new_line)
         is_first_row = True  # flag
+
         for record in csv_to_list_dict(input_file):
             if not is_first_row:
                 serial.write(delimiter + new_line)
             serial.write(indent + '{' + new_line)
             is_first_row = True
+
             for key, value in record.items():
                 if not is_first_row:
                     serial.write(delimiter + new_line)
                 serial.write(f'{indent}{indent}"{key}": {value}')
                 is_first_row = False
+
             serial.write(new_line + indent + '}')
             is_first_row = False
+
         serial.write(new_line + ']')
